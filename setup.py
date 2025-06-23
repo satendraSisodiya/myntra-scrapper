8825c29c102102d40427dbbf7eaf14266a62cc83
satendraSisodiya <satendra.sisodiya07@gmail.com>
2025-06-23 13:10:53 +0530

    create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'scrapper',
+    version = '0.0.1',
+    author = 'satender singh',
+    author_email = 'satendra.sisodiya07@gmail.com',
+    install_requires = [],
+    packages = find_packages()   
+)
